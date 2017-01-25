2b2aeb46b424e10cd56a254a545f532d75be1723
Greg Weiss <gergusa04@gmail.com>
2017-01-25 15:53:50 -0500

    first Part

---
+++ FeaturedCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  FeaturedCollectionViewCell.swift
+//  NacdNews
+//
+//  Created by Gregory Weiss on 10/11/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedCollectionViewCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var featuredImageView: UIImageView!
+    
+    @IBOutlet weak var featuredTitleLabel: UILabel!
+    
+    @IBOutlet weak var featuredDetailsLabel: UILabel!
+    
+    @IBOutlet weak var featuredButton: UIButton!
+    
+    @IBOutlet weak var backingBlurImage: UIImageView!
+    
+    @IBOutlet weak var featuredPlayButton: UIButton!
+    
+    @IBOutlet weak var tabBarButton: UIButton!
+    
+    
+//    override func preferredLayoutAttributesFittingAttributes(layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes! {
+//        let toReturn = super.preferredLayoutAttributesFittingAttributes(layoutAttributes)
+//        return toReturn
+//    }
+    
+//    override func preferredLayoutAttributesFittingAttributes(layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+//        return super.preferredLayoutAttributesFittingAttributes(layoutAttributes)
+//    }
+    
+    //
+    
+}
